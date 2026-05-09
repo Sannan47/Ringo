de4dceb26515195d8caae7414aa0443483057d70
Sannan47 <sannanshakeel04@gmail.com>
2026-05-10 04:48:13 +0500

    tun tun sahoor

--- frontend/hooks/useWebRTC.js
+++ frontend/hooks/useWebRTC.js
@@ -3,7 +3,20 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 const rtcConfig = {
-  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject", 
+      credential: "openrelayproject",
+    },
+  ],
 };
 
 const createEmptyState = () => ({
